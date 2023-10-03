fb1c26fee79988f9319d3fedade6153b657c8fd5
sophia <amibaby104@gmail.com>
2023-10-03 19:46:56 +0100

    codes updated

--- module/ansible/ansible-user.tf
+++ module/ansible/ansible-user.tf
@@ -5,7 +5,7 @@ resource "aws_iam_access_key" "user-access-key" {
   user = aws_iam_user.set_15.name
 }
 resource "aws_iam_group" "developers" {
-  name = "ansible-group"
+  name = "ansible-group" 
 }
 resource "aws_iam_user_group_membership" "team" {
   user = aws_iam_user.set_15.name
@@ -15,3 +15,4 @@ resource "aws_iam_group_policy_attachment" "policy" {
   group = aws_iam_group.developers.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
+
